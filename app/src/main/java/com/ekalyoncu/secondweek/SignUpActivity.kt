08a49c61764b74a23be44824742e07c28b50eb75
Erdem Kalyoncu <ekalyoncu81@gmail.com>
2022-09-27 17:31:50 +0300

    Initial commit

---
+++ app/src/main/java/com/ekalyoncu/secondweek/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.ekalyoncu.secondweek
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
